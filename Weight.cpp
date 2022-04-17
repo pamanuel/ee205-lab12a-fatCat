eba8b69d0bf18f8176403860fef3176e8b0e19f3
pmmanuel315 <88692800+pmmanuel315@users.noreply.github.com>
2022-04-16 14:18:22 -1000

    implement dump function

--- Weight.cpp
+++ Weight.cpp
@@ -8,9 +8,12 @@
 /// @author @Patrick Manuel <pamanuel@hawaii.edu>
 /// @date   14_Apr_2022
 ///////////////////////////////////////////////////////////////////////////////
-
+#include <iostream>
 #include <cassert>
+#include <iomanip>
 #include "Weight.h"
+
+using namespace std;
 
 const float Weight::KILOS_IN_A_POUND = 0.453592;
 const float Weight::SLUGS_IN_A_POUND = 0.031081;
@@ -81,15 +84,16 @@ bool Weight::hasMaxWeight() const noexcept {
 float Weight::getWeight() const noexcept {
     return weight;
 }
+
 float Weight::getWeight(Weight::UnitOfWeight weightUnits) const noexcept {
-    return 0;
+    return 0;///not done
 }
 float Weight::getMaxWeight() const noexcept {
     assert(isWeightValid(maxWeight));
     return maxWeight;
 }
 Weight::UnitOfWeight Weight::getWeightUnit() const noexcept {
-    return Weight::POUND;
+    return Weight::POUND;///not done
 }
 void Weight::setWeight(float newWeight) {
     assert(isWeightValid(newWeight));
@@ -120,7 +124,20 @@ bool Weight::validate() const noexcept {
     assert(isWeightValid(maxWeight));
     return false;
 }
+
+#define FORMAT_LINE( className, member ) cout << setw(8) << (className) << setw(20) << (member) << setw(52)
+
 void Weight::dump() const noexcept {
+    cout << setw(80) << setfill('=' ) << "" << endl ;
+    cout << setfill( ' ' ) ;
+    cout << left ;
+    cout << boolalpha ;
+    FORMAT_LINE( "Weight" , "this" )         << this << endl ;
+    FORMAT_LINE( "Weight", "isKnown" )      << bIsKnown << endl ;
+    FORMAT_LINE( "Weight", "weight" )       << getWeight() << endl ;
+    FORMAT_LINE( "Weight", "unitOfWeight" ) << getWeightUnit() << endl ;
+    FORMAT_LINE( "Weight", "hasMax" )       << bHasMax << endl ;
+    FORMAT_LINE( "Weight", "maxWeight" )    << getMaxWeight() << endl ;
 
 }
 
@@ -138,15 +155,15 @@ float Weight::fromPoundToSlug(float pound) noexcept {
     return pound * SLUGS_IN_A_POUND;
 }
 float Weight::convertWeight(float fromWeight, Weight::UnitOfWeight fromUnit, Weight::UnitOfWeight toUnit) noexcept {
-    return 0;
+    return 0;///not done
 }
 
 ///operators///
 bool Weight::operator==(const Weight &rhs_Weight) const {
-    return false;
+    return false;///not done
 }
 bool Weight::operator<(const Weight &rhs_Weight) const {
-    return false;
+    return false;///not done
 }
-Weight &Weight::operator+=(float rhs_addToWeight) {
+Weight &Weight::operator+=(float rhs_addToWeight) {///not done
 }
